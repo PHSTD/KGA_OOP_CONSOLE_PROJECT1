ff1f3364cdd691d284a9fb2bcf1c3ea1383b8336
PHSTD <c.oadib.autoan123@gmail.com>
2025-04-08 18:15:35 +0900

    [Feat] 던전 작업 중 씬 작업 90% 완료
    모든 기본 씬전환 완료 던전 맵 생성 몬스터 생성 체력 시스템 남음
    이동하는 기능도 추가 필요

--- Scene/DungeonScene.cs
+++ Scene/DungeonScene.cs
@@ -4,12 +4,10 @@ public class DungeonScene : BaseScene
 {
     public override void Render()
     {
-        Console.WriteLine("당신은 광장으로 들어왔습니다.");
-        Console.WriteLine("1. 포션 상점으로 간다");
-        Console.WriteLine("2. 방어구 상점으로 간다");
-        Console.WriteLine("3. 무기 상점으로 간다");
-        Console.WriteLine("4. 던전으로 간다");
-        Console.WriteLine("5. 집으로 간다.");
+        Console.WriteLine("========== 설정 ============");
+        Console.WriteLine("1. 소리설정");
+        Console.WriteLine("2. 그래픽 설정");
+        Console.WriteLine("3. 타이틀로");
     }
 
 
@@ -17,21 +15,7 @@ public class DungeonScene : BaseScene
     {
         switch (inputKey)
         {
-            case ConsoleKey.D1:
-                Console.WriteLine("포션 상점으로 향한다");
-                break;
-            case ConsoleKey.D2:
-                Console.WriteLine("방어구 상점으로 향한다");
-                break;
-            case ConsoleKey.D3:
-                Console.WriteLine("무기 상점으로 향한다");
-                break;
-            case ConsoleKey.D4:
-                Console.WriteLine("던전으로 향한다");
-                break;
-            case ConsoleKey.D5:
-                Console.WriteLine("집으로 간다");
-                break;
+            
         }
     }
 
@@ -39,22 +23,7 @@ public class DungeonScene : BaseScene
     {
         switch (inputKey)
         {
-            case ConsoleKey.D1:
-                Game.ChangeScene("Potion");
-                break;
-            case ConsoleKey.D2:
-                Game.ChangeScene("Potion");
-                Console.WriteLine("방어구 상점으로 향한다");
-                break;
-            case ConsoleKey.D3:
-                Console.WriteLine("무기 상점으로 향한다");
-                break;
-            case ConsoleKey.D4:
-                Console.WriteLine("던전으로 향한다");
-                break;
-            case ConsoleKey.D5:
-                Console.WriteLine("집으로 간다");
-                break;
+            
         }
     }
 }
