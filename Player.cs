82a8b3dd590b7455122c216c9c2fb6dcb9c5d99b
PHSTD <c.oadib.autoan123@gmail.com>
2025-04-09 10:51:38 +0900

    [Feat] Vecter2 추가
    유틸 클래스 추가
    벡터 기능 추가

--- Player.cs
+++ Player.cs
@@ -4,4 +4,26 @@ public static class Player
 {
     public static int hp = 100;
 
+    public static void PlayerSet(int level)
+    {
+        switch (level)
+        {
+            case 1:
+                Console.SetCursorPosition();
+                break;
+            
+            case 2:
+                break;
+            
+            case 3:
+                break;
+            
+            case 4:
+                break;
+            
+            case 5:
+                break;
+        }
+    }
+
 }
---
+++ Util.cs
@@ -0,0 +1,16 @@
+namespace KGA_OOP_CONSOLE_PROJECT1;
+
+public class Util
+{
+    public struct Vectior2
+    {
+        public int x;
+        public int y;
+
+        public Vectior2(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
